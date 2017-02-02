1ffa0e94c52de6be364a565a9455882027d0e95e
Brit Butler <redline6561@gmail.com>
2017-02-02 10:57:06 -0500

    Add initial hangman data.

---
+++ main.js
@@ -0,0 +1,12 @@
+var wordBank = [
+  "coffee", "bourbon", "sleep", "iron",
+  "yard", "challenges", "games", "weekends",
+  "concerts", "pints", "friday", "disestablishmentarianism",
+  "lynx", "falcons", "super", "bowl",
+  "patriots", "winner", "cheat", "commercial"
+];
+var turnCount = 7;
+var guesses = [];
+var answer = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+console.log(answer);
