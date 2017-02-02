aa8f2804a6ac7dead9d8644a9f8b528068ea1792
Brit Butler <redline6561@gmail.com>
2017-02-02 11:57:57 -0500

    Track guesses and recognize incorrect guesses.

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ var turnCount = 7;
 var guesses = [];
 var answer = wordBank[Math.floor(Math.random() * wordBank.length)];
 
+var alphabetBox = document.querySelector(".alphabet");
+alphabetBox.addEventListener('click', processClick);
+
 console.log(answer);
 console.log(answer.length);
 
@@ -21,3 +24,39 @@ function drawBoard (word) {
 }
 
 drawBoard(answer);
+
+function processClick (event) {
+  var target = event.target;
+  var letter = target.innerHTML.toLowerCase();
+  takeTurn(letter);
+  target.classList.add("guessed");
+}
+
+function takeTurn (letter) {
+  // alreadyGuessed should take a letter and return true/false
+  // based on if it is in the guesses array or not
+  if (includes(guesses, letter)) {
+    alert("You already guessed that!");
+  } else {
+    handleGuess(letter);
+  }
+  checkGameOver();
+}
+
+function handleGuess (letter) {
+  guesses.push(letter);
+  if (includes(answer, letter)) {
+    // display the letter
+  } else {
+    turnCount -= 1;
+  }
+}
+
+function includes (container, thing) {
+  for (var count = 0; count < container.length; count++) {
+    if (container[count] === thing) {
+      return true;
+    }
+  }
+  return false;
+}
